851dbfa1ffb07990f58a5816541c630bebbf5cfe
이상용 <lsy3709@naver.com>
2025-08-06 14:25:41 +0900

    5교시, settings.json 파일에 , 자동 정렬 설정

--- hello-react2/src/App.jsx
+++ hello-react2/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <div>
-    <h1>리액트 jsx 문법 처음 해보기</h1>
-    <h2>기본 jsx 문법 규칙, 감싸는 부모 태그가 필요함. 현재 오류가 남. </h2>
+      <h1>리액트 jsx 문법 처음 해보기</h1>
+      <h2>기본 jsx 문법 규칙, 감싸는 부모 태그가 필요함. 현재 오류가 남. </h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
