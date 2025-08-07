12c208f6b58d3a31ff87b515408e83ad7d6123a4
이상용 <lsy3709@naver.com>
2025-08-07 15:38:56 +0900

    6교시, 4장 이벤트 핸들링 작업 준비2

--- ch04-vite-test/src/App.jsx
+++ ch04-vite-test/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import EventPractice from "./component/EventPractice";
 
 function App() {
   return (
     <>
       <h1>ch4 이벤트 핸들링</h1>
+      <h2>1 EventPractice 자식 컴포넌트 호출하기</h2>
+      <EventPractice />
     </>
   );
 }
---
+++ ch04-vite-test/src/component/EventPractice.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  render() {
+    return (
+      <>
+        <h1>이벤트 연습</h1>
+      </>
+    );
+  }
+}
+
+export default EventPractice;
