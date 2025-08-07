62dcd894b1099cca4c5b8049487a51afda887178
이상용 <lsy3709@naver.com>
2025-08-07 15:59:53 +0900

    7교시, 4장 이벤트 핸들링 작업 준비3 onChange

--- ch04-vite-test/src/component/EventPractice.jsx
+++ ch04-vite-test/src/component/EventPractice.jsx
@@ -5,6 +5,15 @@ class EventPractice extends Component {
     return (
       <>
         <h1>이벤트 연습</h1>
+        <h2>onChange 연습 해보기. </h2>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력하세요"
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+        />
       </>
     );
   }
