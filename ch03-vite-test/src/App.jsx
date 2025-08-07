7c6d4913da3f2db33ba9aa7c625cd0e90cdcc719
이상용 <lsy3709@naver.com>
2025-08-07 09:40:38 +0900

    0807 , 1교시, 복습, ch03 , 컴포넌트 개념 준비1

---
+++ ch03-vite-test/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  const name = "오늘 점심 뭐 먹지?";
+  return (
+    <>
+      <h1>ch3 컴포넌트 개념</h1>
+      <div className="react">{name}</div>
+    </>
+  );
+}
+
+export default App;
