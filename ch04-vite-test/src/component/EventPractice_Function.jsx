1558cb706b732448fb2d6732496800e5b51ed382
이상용 <lsy3709@naver.com>
2025-08-08 10:18:45 +0900

    2교시, 함수형 컴포넌트에서, 이벤트 핸들링 연습2

--- ch04-vite-test/src/component/EventPractice_Function.jsx
+++ ch04-vite-test/src/component/EventPractice_Function.jsx
@@ -3,12 +3,20 @@ import { useState } from "react";
 
 const EventPractice_Function = () => {
   const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
 
   const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
 
   const onClick = () => {
-    alert(username + ":");
+    alert(username + ":" + message);
     setUsername("");
+    setMessage("");
   };
 
   return (
@@ -16,15 +24,26 @@ const EventPractice_Function = () => {
       <h1>이벤트 연습, 함수형 컴포넌트 </h1>
       <h2>1 onClick, onChange 연습 해보기. </h2>
       <h3>username : {username}</h3>
+      <h3>message : {message}</h3>
+      <input
+        type="text"
+        name="username"
+        placeholder="username 아무거나 입력하세요"
+        value={username} // getter, state 객체 안의 message 조회
+        onChange={onChangeUsername}
+        // 추가 키 이벤트 리스너, 엔터 키를 입력시, 클릭 하는 효과 주기.
+        onKeyPress={onKeyPress}
+      />
+
       <input
         type="text"
         name="message"
-        placeholder="아무거나 입력하세요"
-        value={username} // getter, state 객체 안의 message 조회
+        placeholder="message 아무거나 입력하세요"
+        value={message} // getter, state 객체 안의 message 조회
         // onClick={onClick}
-        onChange={onChangeUsername}
+        onChange={onChangeMessage}
         // 추가 키 이벤트 리스너, 엔터 키를 입력시, 클릭 하는 효과 주기.
-        //onKeyPress={this.handleKeyPress}
+        onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
     </>
