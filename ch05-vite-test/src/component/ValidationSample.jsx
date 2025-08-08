1562f2ee25e3bedf68ddd8d0a419a308f8805c9c
이상용 <lsy3709@naver.com>
2025-08-08 14:25:36 +0900

    5교시, ch05 , 특정 요소에 접근 하기 위한 방법 ref 테스트 준비2

--- ch05-vite-test/src/component/ValidationSample.jsx
+++ ch05-vite-test/src/component/ValidationSample.jsx
@@ -20,6 +20,18 @@ class ValidationSample extends Component {
       validated: this.state.password === "0000",
     });
   };
+
+  // 특정 DOM 에 접근 하기 위해서, 어쩔수 없이,
+  // ref 라는 속성을 사용하는 데, 1) 포커스 2) 스크롤 부분에 접근.
+  // 포커스 용, 이벤트 핸들러 추가
+
+  // 방법2,
+  input = React.createRef();
+
+  handleFocus = () => {
+    this.input.current.focus();
+  };
+
   render() {
     return (
       <>
@@ -36,8 +48,14 @@ class ValidationSample extends Component {
                   : "failure"
                 : ""
             }
+            //방법1, 콜백 함수 사용, 태그에 접근하기.
+            // ref={(ref) => {
+            //   this.input = ref;
+            // }}
+            ref={this.input}
           />
           <button onClick={this.handleButtonClick}>체크</button>
+          <button onClick={this.handleFocus}>포커스</button>
         </div>
       </>
     );
