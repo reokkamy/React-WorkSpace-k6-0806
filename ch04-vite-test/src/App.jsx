c15aceded54e53284d64af43bee45c16d8b04a2e
이상용 <lsy3709@naver.com>
2025-08-08 11:21:41 +0900

    3교시, 함수형 컴포넌트에서, 회원 가입 형식으로, useState , 이벤트 핸들러 연습 해보기,

--- ch04-vite-test/src/App.jsx
+++ ch04-vite-test/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import EventPractice from "./component/EventPractice";
 import EventPractice_Function from "./component/EventPractice_Function";
+import JoinForm from "./component/JoinForm";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <EventPractice />
       <h2>2 EventPractice_Function, 함수형 컴포넌트 자식 컴포넌트 호출하기</h2>
       <EventPractice_Function />
+      <h2>3 회원 가입 형식으로 연습 해보기, 복습 </h2>
+      <JoinForm />
     </>
   );
 }
