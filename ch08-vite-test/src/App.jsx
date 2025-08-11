da75b4a09f8abe54fd9597b650cd23d106da36d9
이상용 <lsy3709@naver.com>
2025-08-11 14:44:00 +0900

    5교시, 웹스톰 IDE 관련 설정 확인

---
+++ ch08-vite-test/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="react"> ch08 함수형 컴포넌트 hooks</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
