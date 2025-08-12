1680841903236d85f1bda0b1623b8d75bc26d101
이상용 <lsy3709@naver.com>
2025-08-12 11:30:05 +0900

    3교시, hooks, useRef, 특정 태그에 직접 접근하기.

--- ch08-vite-test/src/App.jsx
+++ ch08-vite-test/src/App.jsx
@@ -15,30 +15,30 @@ function App() {
         <h1 className="react"> ch08 함수형 컴포넌트 hooks</h1>
       </div>
       <div>
-        <h2>
-          1 useState , Counter 자식 컴포넌트 호출 또는 그리기 또는 렌더링{' '}
-        </h2>
-        <Counter />
+        {/*<h2>*/}
+        {/*  1 useState , Counter 자식 컴포넌트 호출 또는 그리기 또는 렌더링{' '}*/}
+        {/*</h2>*/}
+        {/*<Counter />*/}
 
-        <button onClick={() => setVisible(!visible)}>
-          {visible ? '숨기기' : '보이기'}
-        </button>
-        <h2>
-          2 useState - useEffect, Info자식 컴포넌트 호출 또는 그리기 또는
-          렌더링{' '}
-        </h2>
-        {visible && <Info />}
+        {/*<button onClick={() => setVisible(!visible)}>*/}
+        {/*  {visible ? '숨기기' : '보이기'}*/}
+        {/*</button>*/}
+        {/*<h2>*/}
+        {/*  2 useState - useEffect, Info자식 컴포넌트 호출 또는 그리기 또는*/}
+        {/*  렌더링{' '}*/}
+        {/*</h2>*/}
+        {/*{visible && <Info />}*/}
 
-        <h2>3 useReducer , CounteUseReducer , 상태 관리 hook 사용해보기,</h2>
-        <CountUseReducer />
+        {/*<h2>3 useReducer , CounteUseReducer , 상태 관리 hook 사용해보기,</h2>*/}
+        {/*<CountUseReducer />*/}
 
-        <h2>3-2 useReducer , InfoUseReducer , 상태 관리 hook 사용해보기,</h2>
-        <InfoUseReducer />
+        {/*<h2>3-2 useReducer , InfoUseReducer , 상태 관리 hook 사용해보기,</h2>*/}
+        {/*<InfoUseReducer />*/}
 
-        <h2>
-          4 useMemo , Average , 무거운 연산의 결과값을 기억해서 재사용해보자.,
-        </h2>
-        <Average />
+        {/*<h2>*/}
+        {/*  4 useMemo , Average , 무거운 연산의 결과값을 기억해서 재사용해보자.,*/}
+        {/*</h2>*/}
+        {/*<Average />*/}
 
         <h2>5 useCallback , Average , 함수를 기억해서 재사용해보자.,</h2>
         <AverageUseCallback />
