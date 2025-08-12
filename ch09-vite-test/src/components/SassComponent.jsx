f0f0285ccf2f669c87275256209e8686ad4d5e9a
이상용 <lsy3709@naver.com>
2025-08-12 15:17:46 +0900

    6교시, ch9 , 리액트 스타일링 기본 확인, 1 sass 기본 문법 확인4, 무지개 박스 확인

--- ch09-vite-test/src/components/SassComponent.jsx
+++ ch09-vite-test/src/components/SassComponent.jsx
@@ -6,6 +6,12 @@ const SassComponent = () => {
     <div>
       <div className="SassComponent">
         <div className="box red" />
+        <div className="box orange" />
+        <div className="box yellow" />
+        <div className="box green" />
+        <div className="box blue" />
+        <div className="box indigo" />
+        <div className="box violet" />
       </div>
     </div>
   );
