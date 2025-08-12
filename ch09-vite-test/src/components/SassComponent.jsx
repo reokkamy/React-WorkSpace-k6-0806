dd054612aed21e486930443dd82b1fc1ea8ee3fe
이상용 <lsy3709@naver.com>
2025-08-12 15:07:48 +0900

    6교시, ch9 , 리액트 스타일링 기본 확인, 1 sass 기본 문법 확인

---
+++ ch09-vite-test/src/components/SassComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/SassComponent.scss';
+
+const SassComponent = () => {
+  return (
+    <div>
+      <div className="SassComponent">
+        <div className="box red" />
+      </div>
+    </div>
+  );
+};
+
+export default SassComponent;
