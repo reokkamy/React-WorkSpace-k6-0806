0f7b5915e35988996465630e0766461475b9d0c9
이상용 <lsy3709@naver.com>
2025-08-12 16:07:25 +0900

    7교시, ch9 , 리액트 스타일링 기본 확인, 3 CSSModule2

--- ch09-vite-test/src/components/CSSModule.jsx
+++ ch09-vite-test/src/components/CSSModule.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/CSSModule.module.css';
 const CssModule = () => {
   return (
     <div>
-      <div className={styles.wrapper}>
+      <div className={`${styles.wrapper} ${styles.inverted}`}>
         <span className="something">Hello CSSModule~~~,</span> 클래스명을
         중복되지 않게 사용해 주는 도구.
       </div>
