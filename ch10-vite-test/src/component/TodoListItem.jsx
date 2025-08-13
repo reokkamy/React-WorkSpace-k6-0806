09d1e14b79539eb0ea47d385d8393eb938161b7d
이상용 <lsy3709@naver.com>
2025-08-13 15:19:48 +0900

    6교시, ch10, 일정 관리 미니 프로젝트 작업 기능 구현 작업중, todo 글쓰기 수정 작업

--- ch10-vite-test/src/component/TodoListItem.jsx
+++ ch10-vite-test/src/component/TodoListItem.jsx
@@ -10,12 +10,12 @@ import {
 import '../styles/TodoListItem.scss';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
   const { id, text, checked } = todo;
   return (
     <div className="TodoListItem">
       {/*classnames , cn 별칭 지정하고, 조건부 렌더링 적용하기.*/}
-      <div className={cn('checkbox', { checked })}>
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
