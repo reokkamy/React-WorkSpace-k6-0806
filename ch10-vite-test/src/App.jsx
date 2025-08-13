a7bd883d7cbd2ad530ba189b2e06caf573f1319e
이상용 <lsy3709@naver.com>
2025-08-13 10:37:23 +0900

    2교시, ch10, 일정 관리 미니 프로젝트 준비3, TodoTemplate.jsx

--- ch10-vite-test/src/App.jsx
+++ ch10-vite-test/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import TodoTemplate from './component/TodoTemplate.jsx';
 
 function App() {
   return (
     <>
       <h1 className="react">ch10 일정 관리 미니 프로젝트 </h1>
+      <TodoTemplate>Todo App 만들기!!</TodoTemplate>
     </>
   );
 }
