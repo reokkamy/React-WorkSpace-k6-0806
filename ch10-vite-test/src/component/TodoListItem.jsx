f2239609261f4d12a3c1c20ed7ef419a71d713f9
이상용 <lsy3709@naver.com>
2025-08-13 11:50:45 +0900

    3교시, ch10, 일정 관리 미니 프로젝트 작업 TodoList.jsx, TodoListItem.jsx, css 작업 하기

---
+++ ch10-vite-test/src/component/TodoListItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+//<MdCheckBoxOutlineBlank />
+//<MdCheckBox />
+// <MdRemoveCircle />
+import '../styles/TodoListItem.scss';
+const TodoListItem = () => {
+  return (
+    <div className="TodoListItem">
+      <div className="checkbox">
+        <MdCheckBoxOutlineBlank />
+        <div className="text">할 일</div>
+      </div>
+      <div className="remove">
+        {' '}
+        <MdCheckBoxOutlineBlank />
+      </div>
+    </div>
+  );
+};
+
+export default TodoListItem;
