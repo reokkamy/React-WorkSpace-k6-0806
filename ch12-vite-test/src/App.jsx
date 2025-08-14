d410fa7ce9c335934d42e8078d24671efae3acaa
이상용 <lsy3709@naver.com>
2025-08-14 11:35:53 +0900

    3교시,Immer 도구 사용 준비1

---
+++ ch12-vite-test/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="react">
+        ch12, 불변성 유지, 가독성 있는 Immer 라이브러리 사용{' '}
+      </h1>
+    </>
+  );
+}
+
+export default App;
