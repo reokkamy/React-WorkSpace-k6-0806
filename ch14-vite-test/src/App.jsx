d0791605a103ef14f1e81fcbc2e242d2740d5d8e
mj <quinate@kakao.com>
2025-08-18 16:07:40 +0900

    8/12

--- ch14-vite-test/src/App.jsx
+++ ch14-vite-test/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
-import NewsList from './components/NewsList.jsx';
 
 function App() {
   const [data, setData] = useState(null);
@@ -16,8 +15,8 @@ function App() {
   const onClick = async () => {
     try {
       const response = await axios.get(
-        // 'http://jsonplaceholder.typicode.com/todos/1',
-        'https://newsapi.org/v2/top-headlines?country=us&apiKey=본인꺼',
+        '\n' +
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=2817433b685d41b8af6650bb66142986',
       );
       setData(response.data);
     } catch (e) {
@@ -27,19 +26,18 @@ function App() {
   return (
     <>
       <h1 className="react">ch14 REST API 연결 연습</h1>
-      {/*<div>*/}
-      {/*  <button onClick={onClick}>샘플 데이터 불러오기 테스트</button>*/}
-      {/*</div>*/}
-      {/*<div>*/}
-      {/*  {data && (*/}
-      {/*    <textarea*/}
-      {/*      rows={7}*/}
-      {/*      value={JSON.stringify(data, null, 2)}*/}
-      {/*      readOnly={true}*/}
-      {/*    />*/}
-      {/*  )}*/}
-      {/*</div>*/}
-      <NewsList />
+      <div>
+        <button onClick={onClick}>샘플 데이터 불러오기 테스트</button>
+      </div>
+      <div>
+        {data && (
+          <textarea
+            rows={7}
+            value={JSON.stringify(data, null, 2)}
+            readOnly={true}
+          />
+        )}
+      </div>
     </>
   );
 }
