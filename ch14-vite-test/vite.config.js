9e2652b24c136fdfb7214c4da039a41f9c37d60b
reokkamy <quinate@kakao.com>
2025-08-20 22:55:33 +0900

    ㅇ녀습

--- ch14-vite-test/vite.config.js
+++ ch14-vite-test/vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true, // 브라우저 자동 열기
-    proxy: {
-      '/api': {
-        target: 'https://apis.data.go.kr',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: true,
-      },
-    },
-  },
+    server: {
+        proxy: {
+            '/api/humetro': {
+                target: 'http://data.humetro.busan.kr',
+                changeOrigin: true,
+                rewrite: (p) => p.replace(/^\/api\/humetro/, ''),
+            },
+        },
+    }
+
+
 });
+
